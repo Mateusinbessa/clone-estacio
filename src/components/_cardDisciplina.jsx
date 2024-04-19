a4bfa3dad34d9b8423814d46400b4bc4c7287941
Mateus Bessa <mateusbessacontato@gmail.com>
2024-04-19 10:50:58 -0300

    updates

--- src/components/_cardDisciplina.jsx
+++ src/components/_cardDisciplina.jsx
@@ -1,37 +1,33 @@
-import ArrowRight from 'src/assets/arrowright.png'
-import Maps from 'src/assets/maps.png'
-import Porta from 'src/assets/porta.png'
-import Calendario from 'src/assets/calendario.png'
-import Pessoa from 'src/assets/pessoa.png'
+import { ArrowRight, Calendar, DoorOpen, MapPin, User } from 'lucide-react'
 
-const CardDisciplina = ({ disciplina, horario, polo, sala, professor, codTurma, cod, certificadora }) => {
+const CardDisciplina = ({ disciplina, horario, polo, sala, professor, codTurma, cod }) => {
     return (
         <div className="xl:max-w-lg w-full bg-white shadow-lg rounded-lg overflow-hidden mt-2">
             <div className="px-7 py-7 flex flex-col gap-5">
-                <div className='flex justify-between items-center'>
-                    <h2 className="text-gray-800 text-3xl font-semibold sm:max-w-sm">{disciplina}</h2>
-                    <div className='w-10 h-10 cursor-pointer'>
+                <div className='flex justify-between items-center gap-2'>
+                    <h2 className="text-gray-800 text-3xl max-lg:text-xl font-semibold sm:max-w-sm">{disciplina}</h2>
+                    <div className='w-10 h-10 max-lg:w-6 max-lg:h-6 cursor-pointer'>
                         <a href="/auth/details">
-                            <img src={ArrowRight} />
+                            <ArrowRight />
                         </a>
                     </div>
                 </div>
-                <div className='flex gap-2 items-center ml-2'>
-                    <img className='w-6' src={Calendario} alt="" />
+                <div className='flex gap-2 items-center'>
+                    <Calendar />
                     <p className='text-slate-700'>{horario}</p>
                 </div>
                 <div className='flex justify-between'>
                     <div className='flex gap-2 justify-center items-center'>
-                        <img className='w-8' src={Maps} alt="" />
+                        <MapPin />
                         <p className='text-slate-700'>{polo}</p>
                     </div>
                     <div className='flex gap-2 justify-center items-center'>
-                        <img className='w-7' src={Porta} alt="" />
+                        <DoorOpen />
                         <p className='text-slate-700'>Sala {sala}</p>
                     </div>
                 </div>
                 <div className='flex gap-2 items-center'>
-                    <img className='w-8' src={Pessoa} alt="" />
+                    <User />
                     <p className='text-slate-700'>Professor(a) {professor}</p>
                 </div>
                 <div className='flex gap-2'>
@@ -41,11 +37,6 @@ const CardDisciplina = ({ disciplina, horario, polo, sala, professor, codTurma, 
                     <div className='bg-gray-300 rounded-full px-3 py-1'>
                         <p className='text-sm'>{cod}</p>
                     </div>
-                    {certificadora && (
-                        <div className='bg-yellow-200 rounded-full px-3 py-1'>
-                            <p className='text-sm'>Disciplina ceriticadora</p>
-                        </div>
-                    )}
                 </div>
             </div>
         </div>
--- src/pages/private/content.jsx
+++ src/pages/private/content.jsx
@@ -1,14 +1,13 @@
-import ArrowLeft from 'src/assets/arrowleft.png'
 import Capa from 'src/assets/capa.png'
-import Row from 'src/assets/row.png'
 import { useState } from 'react'
+import { ArrowLeft, Minus } from 'lucide-react'
 
 const NavBar = ({ visualized }) => {
   return (
     <nav className="flex justify-between gap-4 sticky top-0 bg-white p-5">
       <div className='flex items-center gap-2'>
         <a href="/auth/details">
-          <img className='w-7' src={ArrowLeft} alt="" />
+          <ArrowLeft />
         </a>
         <p className='text-xl max-lg:text-lg max-md:text-base max-sm:text-sm'>Voltar para disciplina</p>
       </div>
@@ -20,8 +19,8 @@ const NavBar = ({ visualized }) => {
 
 const TitleContent = ({ title }) => {
   return (
-    <div className='flex gap-4'>
-      <img src={Row} alt="" />
+    <div className='flex gap-4 items-center'>
+      <Minus/>
       <p className='text-4xl'>{title}</p>
     </div>
   )
